586479d476f891956d64cc40f1d8d81c3986b560
Anthony Schultz <34993720+georgeschultz1998@users.noreply.github.com>
2022-11-18 07:19:36 +0000

    Allows user to move around map

--- Code/Graveyard.java
+++ Code/Graveyard.java
@@ -72,18 +72,71 @@ class Main {
     Collections.addAll(row5, row5Array);
   }
   
+  static void outputMap(List<List<String>> map) {
+    map.get(0).forEach(System.out::print);
+    System.out.println();
+    map.get(1).forEach(System.out::print);
+    System.out.println();
+    map.get(2).forEach(System.out::print);
+    System.out.println();
+    map.get(3).forEach(System.out::print);
+    System.out.println();
+    map.get(4).forEach(System.out::print);
+  }
+  
+  static void updateLocations(List<List<String>> map, String userAnswer){
+    String[] user = { "-----------", "|    O    |", "|   /|\\   |", "|   / \\   |", "|   USER  |", "|         |",
+        "-----------" };
+    String[] empty = { "-----------", "|         |", "|         |", "|         |", "|         |", "|         |",
+        "-----------" };
 
-  static void outputRoom(List<String> row1, List<String> row2, List<String> row3, List<String> row4,
-      List<String> row5) {
-    row1.forEach(System.out::print);
-    System.out.println();
-    row2.forEach(System.out::print);
-    System.out.println();
-    row3.forEach(System.out::print);
-    System.out.println();
-    row4.forEach(System.out::print);
-    System.out.println();
-    row5.forEach(System.out::print);
+    Integer mapIndex = 0;
+    Integer rowIndex = 0;
+    // Finds current location of user.
+    for (int i = 0; i < 5; i++) {
+    rowIndex = map.get(i).indexOf("|   USER  |");
+      if(rowIndex != -1){
+        mapIndex = i;
+        i=5;
+      }
+    }
+
+    // Updates current user location so it is empty now
+    //System.out.println("That is located on map index: " + mapIndex + ", row index: " + rowIndex);
+    //System.out.println(map.get(mapIndex).get(rowIndex));
+
+    map.get(mapIndex).set(rowIndex-30, empty[0]);
+    map.get(mapIndex).set(rowIndex-18, empty[1]);
+    map.get(mapIndex).set(rowIndex-12, empty[2]);
+    map.get(mapIndex).set(rowIndex-6, empty[3]);
+    map.get(mapIndex).set(rowIndex, empty[4]);
+    map.get(mapIndex).set(rowIndex+6, empty[5]);
+    map.get(mapIndex).set(rowIndex+12, empty[6]);
+
+    
+    // Finds which location to place user at based on their reponse.
+    for (int i = 0; i < 5; i++) {
+    rowIndex = map.get(i).indexOf("|" + userAnswer + "       |");
+      if(rowIndex != -1){
+        mapIndex = i;
+        i=5;
+      }
+    }
+
+    // Updates chosen location so user is there now.
+    //System.out.println("That is located on map index: " + mapIndex + ", row index: " + rowIndex);
+    //System.out.println(map.get(mapIndex).get(rowIndex));
+    map.get(mapIndex).set(rowIndex-6, user[0]);
+    map.get(mapIndex).set(rowIndex+6, user[1]);
+    map.get(mapIndex).set(rowIndex+12, user[2]);
+    map.get(mapIndex).set(rowIndex+18, user[3]);
+    map.get(mapIndex).set(rowIndex+24, user[4]);
+    map.get(mapIndex).set(rowIndex+30, user[5]);
+    map.get(mapIndex).set(rowIndex+36, user[6]);
+
+
+    // Removes 
+    outputMap(map);
   }
 
   public static void main(String[] args) {
@@ -93,14 +146,40 @@ class Main {
     List<String> row3 = new ArrayList<>();
     List<String> row4 = new ArrayList<>();
     List<String> row5 = new ArrayList<>();
+    List<List<String>> map = new ArrayList<>();
     List<String>  location = new ArrayList<>();
 
+    map.add(row1);
+    map.add(row2);
+    map.add(row3);
+    map.add(row4);
+    map.add(row5);
     createRoom(row1, row2, row3, row4, row5, location);
-    outputRoom(row1, row2, row3, row4, row5);
+    outputMap(map);
+    
     Scanner scan = new Scanner(System.in); // Create a Scanner object
-    System.out.println("\nWhere would you like to move to?");
+    System.out.println("\nWhat would you like to do?");
+    System.out.println("Enter 'M' to move your player.");
     String userAnswer = scan.nextLine();
-    System.out.println("User will move to: " + userAnswer); // Output user input
+    userAnswer = userAnswer.toUpperCase();
+    while (userAnswer != "Q"){
+      if (userAnswer.equals("M"))
+      {
+        System.out.println("Enter location to move to:");
+        userAnswer = scan.nextLine();
+        userAnswer = userAnswer.toUpperCase();
+        System.out.println("User will move to: " + userAnswer); 
+        updateLocations(map, userAnswer);
+      }
 
+    System.out.println("\nWhat would you like to do?");
+    System.out.println("Enter 'M' to move your player.");
+    userAnswer = scan.nextLine();
+    userAnswer = userAnswer.toUpperCase();
+    }
+    
+    //Integer locIndex = location.indexOf("|" + userAnswer + "       |");
+    //System.out.println("Location index of that is " + locIndex);
+    
   }
 }
