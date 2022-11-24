e8f90c0ebbd30212f34d85ca411e3660c23e7206
s-full <60407568+s-full@users.noreply.github.com>
2022-11-24 13:03:54 -0800

    Began playable character party creation logic.

--- characters/CharacterGenerator.java
+++ characters/CharacterGenerator.java
@@ -1,5 +1,8 @@
 package characters;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CharacterGenerator {
 
     public static void main(String[] args) {
@@ -12,5 +15,29 @@ public class CharacterGenerator {
         System.out.println(brute.toString());
 
         System.out.println(brute.deck.getDeck());
+
+        System.out.println("Enter the number of characters in the party");
+    }
+
+    public static ArrayList<PlayableCharacter> makeCharacters() {
+        Scanner input = new Scanner(System.in);
+        ArrayList<PlayableCharacter> party = new ArrayList<PlayableCharacter>();
+        System.out.println("Enter the number of characters into your party (1-4):");
+        int partySize = input.nextInt();
+
+        while (partySize > 4 || partySize < 0) {
+            System.out.println("Invalid input...");
+            System.out.println("Enter the number of characters into your party (1-4):");
+            partySize = input.nextInt();
+        }
+
+
+        for (int i = 0; i < partySize; i++) {
+            String charType = "";
+            System.out.println("Please choose the class of the current character (1-3)");
+            System.out.println("Current characters are (1)Brute, (2)Cragheart, (3)Tinker:");
+
+        }
+        return party;
     }
 }
--- characters/PlayableCharacter.java
+++ characters/PlayableCharacter.java
@@ -1,5 +1,5 @@
 package characters;
-import Deck.Deck;
+import Deck.*;
 
 import characters.CharacterInterface;
 
