a1d65a90c8c22287b0fb04c2109c1661d3705fce
Anthony Schultz <34993720+georgeschultz1998@users.noreply.github.com>
2022-11-25 06:43:09 +0000

    Updated Map so it fits on screen better

--- Code/Graveyard.java
+++ Code/Graveyard.java
@@ -2,321 +2,36 @@ import java.util.*;
 
 class Main {
     static void createRoom(List < String > row1, List < String > row2, List < String > row3, List < String > row4,
-        List < String > row5, List < String > location) {
+        List < String > row5, List < String > row6, List < String > location) {
 
-        String[] empty = {
-            "-----------",
-            "|         |",
-            "|         |",
-            "|         |",
-            "|         |",
-            "|         |",
-            "-----------"
-        };
-        String[] bandit = {
-            "-----------",
-            "|    O    |",
-            "|   /|\\   |",
-            "|   / \\   |",
-            "|  BANDIT |",
-            "|         |",
-            "-----------"
-        };
-        String[] livingUndead = {
-            "-----------",
-            "|    O    |",
-            "|   /|\\   |",
-            "|   / \\   |",
-            "|  LIVING |",
-            "|  UNDEAD |",
-            "-----------"
-        };
-        String[] user = {
-            "-----------",
-            "|    O    |",
-            "|   /|\\   |",
-            "|   / \\   |",
-            "|   USER  |",
-            "|         |",
-            "-----------"
-        };
+        String hex = "⬣";
+        String user = "👤";
+      String undead = "👻";
+      String bandit = "😡";
 
-        String[] locationArray = {
-            "|A1       |",
-            "|A2       |",
-            "|A3       |",
-            "|A4       |",
-            "|A5       |",
-            "|B1       |",
-            "|B2       |",
-            "|B3       |",
-            "|B4       |",
-            "|B5       |",
-            "|C1       |",
-            "|C2       |",
-            "|C3       |",
-            "|C4       |",
-            "|C5       |",
-            "|D1       |",
-            "|D2       |",
-            "|D3       |",
-            "|D4       |",
-            "|D5       |",
-            "|E1       |",
-            "|E2       |",
-            "|E3       |",
-            "|E4       |",
-            "|E5       |"
-        };
-        Collections.addAll(location, locationArray);
+        
         String[] row1Array = {
-            empty[0],
-            empty[0],
-            empty[0],
-            empty[0],
-            empty[0],
-            "\n",
-            location.get(0),
-            location.get(1),
-            location.get(2),
-            location.get(3),
-            location.get(4),
-            "\n",
-            empty[1],
-            empty[1],
-            empty[1],
-            empty[1],
-            empty[1],
-            "\n",
-            empty[2],
-            empty[2],
-            empty[2],
-            empty[2],
-            empty[2],
-            "\n",
-            empty[3],
-            empty[3],
-            empty[3],
-            empty[3],
-            empty[3],
-            "\n",
-            empty[4],
-            empty[4],
-            empty[4],
-            empty[4],
-            empty[4],
-            "\n",
-            empty[5],
-            empty[5],
-            empty[5],
-            empty[5],
-            empty[5],
-            "\n",
-            empty[6],
-            empty[6],
-            empty[6],
-            empty[6],
-            empty[6]
+          "|   " , "X1", " " ,"X2", " " ,"X3", " " ,"X4", "|" 
         };
 
         String[] row2Array = {
-            empty[0],
-            empty[0],
-            empty[0],
-            empty[0],
-            empty[0],
-            "\n",
-            location.get(5),
-            location.get(6),
-            location.get(7),
-            location.get(8),
-            location.get(9),
-            "\n",
-            bandit[1],
-            empty[1],
-            empty[1],
-            bandit[1],
-            empty[1],
-            "\n",
-            bandit[2],
-            empty[2],
-            empty[2],
-            bandit[2],
-            empty[2],
-            "\n",
-            bandit[3],
-            empty[3],
-            empty[3],
-            bandit[3],
-            empty[3],
-            "\n",
-            bandit[4],
-            empty[4],
-            empty[4],
-            bandit[4],
-            empty[4],
-            "\n",
-            bandit[5],
-            empty[5],
-            empty[5],
-            bandit[5],
-            empty[5],
-            "\n",
-            bandit[6],
-            empty[6],
-            empty[6],
-            bandit[6],
-            empty[6]
+          "|Y1" + " ", hex + "  ", hex + "  ", hex + "  ", hex + " |"
         };
 
         String[] row3Array = {
-            empty[0],
-            empty[0],
-            livingUndead[0],
-            empty[0],
-            empty[0],
-            "\n",
-            location.get(10),
-            location.get(11),
-            location.get(12),
-            location.get(13),
-            location.get(14),
-            "\n",
-            empty[1],
-            empty[1],
-            livingUndead[1],
-            empty[1],
-            empty[1],
-            "\n",
-            empty[2],
-            empty[2],
-            livingUndead[2],
-            empty[2],
-            empty[2],
-            "\n",
-            empty[3],
-            empty[3],
-            livingUndead[3],
-            empty[3],
-            empty[3],
-            "\n",
-            empty[4],
-            empty[4],
-            livingUndead[4],
-            empty[4],
-            empty[4],
-            "\n",
-            empty[5],
-            empty[5],
-            livingUndead[5],
-            empty[5],
-            empty[5],
-            "\n",
-            empty[6],
-            empty[6],
-            livingUndead[6],
-            empty[6],
-            empty[6]
+          "|Y2" + " ", hex + "  ", undead + " ", hex + "  ", hex + " |"
         };
 
         String[] row4Array = {
-            empty[0],
-            empty[0],
-            empty[0],
-            empty[0],
-            empty[0],
-            "\n",
-            location.get(15),
-            location.get(16),
-            location.get(17),
-            location.get(18),
-            location.get(19),
-            "\n",
-            empty[1],
-            empty[1],
-            empty[1],
-            empty[1],
-            empty[1],
-            "\n",
-            empty[2],
-            empty[2],
-            empty[2],
-            empty[2],
-            empty[2],
-            "\n",
-            empty[3],
-            empty[3],
-            empty[3],
-            empty[3],
-            empty[3],
-            "\n",
-            empty[4],
-            empty[4],
-            empty[4],
-            empty[4],
-            empty[4],
-            "\n",
-            empty[5],
-            empty[5],
-            empty[5],
-            empty[5],
-            empty[5],
-            "\n",
-            empty[6],
-            empty[6],
-            empty[6],
-            empty[6],
-            empty[6]
+          "|Y3" + " ", hex + "  ", hex + "  ", undead + " ", hex + " |"
         };
 
         String[] row5Array = {
-            empty[0],
-            empty[0],
-            user[0],
-            empty[0],
-            empty[0],
-            "\n",
-            location.get(20),
-            location.get(21),
-            location.get(22),
-            location.get(23),
-            location.get(24),
-            "\n",
-            empty[1],
-            empty[1],
-            user[1],
-            empty[1],
-            empty[1],
-            "\n",
-            empty[2],
-            empty[2],
-            user[2],
-            empty[2],
-            empty[2],
-            "\n",
-            empty[3],
-            empty[3],
-            user[3],
-            empty[3],
-            empty[3],
-            "\n",
-            empty[4],
-            empty[4],
-            user[4],
-            empty[4],
-            empty[4],
-            "\n",
-            empty[5],
-            empty[5],
-            user[5],
-            empty[5],
-            empty[5],
-            "\n",
-            empty[6],
-            empty[6],
-            user[6],
-            empty[6],
-            empty[6]
+          "|Y4" + " ", hex + "  ", hex + "  ", hex + "  ", hex + " |"
+        };
+      
+        String[] row6Array = {
+          "|Y5" + " ", user + " ", hex + "  ", hex + "  ", hex + " |"
         };
 
         Collections.addAll(row1, row1Array);
@@ -324,9 +39,12 @@ class Main {
         Collections.addAll(row3, row3Array);
         Collections.addAll(row4, row4Array);
         Collections.addAll(row5, row5Array);
+        Collections.addAll(row6, row6Array);
     }
 
     static void outputMap(List < List < String >> map) {
+      System.out.println("_______________");
+      System.out.println("|      MAP     |");
         map.get(0).forEach(System.out::print);
         System.out.println();
         map.get(1).forEach(System.out::print);
@@ -336,94 +54,32 @@ class Main {
         map.get(3).forEach(System.out::print);
         System.out.println();
         map.get(4).forEach(System.out::print);
+      System.out.println();
+        map.get(5).forEach(System.out::print);
+      System.out.println();
+      System.out.println("_______________ ");
     }
 
-    static void updateLocations(List < List < String >> map, String userAnswer) {
-        String[] user = {
-            "-----------",
-            "|    O    |",
-            "|   /|\\   |",
-            "|   / \\   |",
-            "|   USER  |",
-            "|         |",
-            "-----------"
-        };
-        String[] empty = {
-            "-----------",
-            "|         |",
-            "|         |",
-            "|         |",
-            "|         |",
-            "|         |",
-            "-----------"
-        };
-
-        Integer mapIndex = 0;
-        Integer rowIndex = 0;
-        String correctedAnswer = userAnswer;
+    static void updateLocations(List < List < String >> map, String [] mapCord) {
+      Integer currentXCord = -1;
+      Integer xCord = Integer.parseInt(mapCord[0]);
+      Integer yCord = Integer.parseInt(mapCord[1]);
         Scanner scan = new Scanner(System.in); // Create a Scanner object
-        for (int i = 0; i < 5; i++) {
-            rowIndex = map.get(i).indexOf("|" + correctedAnswer + "       |");
-            if (rowIndex != -1) {
-                mapIndex = i;
-                while (map.get(mapIndex).get(rowIndex + 6).equals("|    O    |")) {
-                    //System.out.println(map.get(mapIndex).get(rowIndex+6));
-                    //System.out.println("|" + correctedAnswer + "       |");
-                    System.out.println("Invalid Entry, another character already located there.");
-                    System.out.println("Enter location to move to:");
-                    correctedAnswer = scan.nextLine();
-                    correctedAnswer = correctedAnswer.toUpperCase();
-                    rowIndex = map.get(i).indexOf("|" + correctedAnswer + "       |");
+      //System.out.println(map.get(yCord).get(xCord));
+      if (map.get(yCord).get(xCord).equals("⬣  ")){
 
-                }
-            }
+        for (int i = 0; i < map.size(); i++) {
+        currentXCord = map.get(i).indexOf("👤 ");
+          if (!(currentXCord.equals(-1))){
+            map.get(i).set(currentXCord, "⬣  ");
+            i = map.size();
+          }
         }
-        userAnswer = correctedAnswer;
-
-        mapIndex = 0;
-        rowIndex = 0;
-        // Finds current location of user.
-        for (int i = 0; i < 5; i++) {
-            rowIndex = map.get(i).indexOf("|   USER  |");
-            if (rowIndex != -1) {
-                mapIndex = i;
-                i = 5;
-            }
-        }
-
-        // Updates current user location so it is empty now
-        //System.out.println("That is located on map index: " + mapIndex + ", row index: " + rowIndex);
-        //System.out.println(map.get(mapIndex).get(rowIndex));
-
-        map.get(mapIndex).set(rowIndex - 30, empty[0]);
-        map.get(mapIndex).set(rowIndex - 18, empty[1]);
-        map.get(mapIndex).set(rowIndex - 12, empty[2]);
-        map.get(mapIndex).set(rowIndex - 6, empty[3]);
-        map.get(mapIndex).set(rowIndex, empty[4]);
-        map.get(mapIndex).set(rowIndex + 6, empty[5]);
-        map.get(mapIndex).set(rowIndex + 12, empty[6]);
-
-
-        // Finds which location to place user at based on their reponse.
-        for (int i = 0; i < 5; i++) {
-            rowIndex = map.get(i).indexOf("|" + userAnswer + "       |");
-            if (rowIndex != -1) {
-                mapIndex = i;
-                i = 5;
-            }
-        }
-
-        // Updates chosen location so user is there now.
-        //System.out.println("That is located on map index: " + mapIndex + ", row index: " + rowIndex);
-        //System.out.println(map.get(mapIndex).get(rowIndex));
-        map.get(mapIndex).set(rowIndex - 6, user[0]);
-        map.get(mapIndex).set(rowIndex + 6, user[1]);
-        map.get(mapIndex).set(rowIndex + 12, user[2]);
-        map.get(mapIndex).set(rowIndex + 18, user[3]);
-        map.get(mapIndex).set(rowIndex + 24, user[4]);
-        map.get(mapIndex).set(rowIndex + 30, user[5]);
-        map.get(mapIndex).set(rowIndex + 36, user[6]);
-
+        map.get(yCord).set(xCord, "👤 ");
+      }
+      else{
+         System.out.println("Cannot move there, please choose an empty space.");
+      }
 
         // Removes 
         outputMap(map);
@@ -436,6 +92,7 @@ class Main {
         List < String > row3 = new ArrayList < > ();
         List < String > row4 = new ArrayList < > ();
         List < String > row5 = new ArrayList < > ();
+        List < String > row6 = new ArrayList < > ();
         List < List < String >> map = new ArrayList < > ();
         List < String > location = new ArrayList < > ();
 
@@ -444,24 +101,31 @@ class Main {
         map.add(row3);
         map.add(row4);
         map.add(row5);
-        createRoom(row1, row2, row3, row4, row5, location);
+        map.add(row6);
+        createRoom(row1, row2, row3, row4, row5, row6, location);
         outputMap(map);
 
         Scanner scan = new Scanner(System.in); // Create a Scanner object
-        System.out.println("\nWhat would you like to do?");
+        System.out.println("");
+        System.out.println("Menu Options:");
         System.out.println("Enter 'M' to move your player.");
+        System.out.println("Enter 'Q' to quit.");
+      
         String userAnswer = scan.nextLine();
         userAnswer = userAnswer.toUpperCase();
-        while (userAnswer != "Q") {
+        while (!(userAnswer.equals("Q"))) {
             if (userAnswer.equals("M")) {
-                System.out.println("Enter location to move to:");
+                System.out.println("Enter location to move to: (Example: 1,4 would move you to position X1 Y4)");
                 userAnswer = scan.nextLine();
                 userAnswer = userAnswer.toUpperCase();
-                System.out.println("User will move to: " + userAnswer);
-                updateLocations(map, userAnswer);
+                String [] mapCord = userAnswer.split("[,]", 0);
+                updateLocations(map, mapCord);
+                System.out.println("");
+                System.out.println("User has been moved to: " + userAnswer);
             }
-            System.out.println("\nWhat would you like to do?");
+            System.out.println("Menu Options:");
             System.out.println("Enter 'M' to move your player.");
+            System.out.println("Enter 'Q' to quit.");
             userAnswer = scan.nextLine();
             userAnswer = userAnswer.toUpperCase();
         }
