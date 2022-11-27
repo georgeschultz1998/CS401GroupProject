6f56161bddb81f67ff3f00126ad480aa25d273d5
Anthony Schultz <34993720+georgeschultz1998@users.noreply.github.com>
2022-11-26 19:59:50 -0500

    Fixed Error Message

---
+++ heptalionConsoleMVC/consoleView/Game.java
@@ -0,0 +1,182 @@
+package heptalionConsoleMVC.consoleView;
+
+import com.sun.javafx.scene.traversal.Direction;
+import heptalionConsoleMVC.heptalion.Board;
+import heptalionConsoleMVC.heptalion.Deck;
+import heptalionConsoleMVC.heptalion.Domino;
+import heptalionConsoleMVC.heptalion.Player;
+
+import java.util.Scanner;
+
+/**
+ * Heptalion main game class
+ * @author David
+ */
+public class Game {
+    Board board;
+    Player[] players;
+    Deck deck;
+    private Scanner stdin = new Scanner(System.in);
+    static final int NUM_PLAYERS = 2;
+    private static final int MAX_TRIES = 3;
+    private static final String QUIT = "Q";
+    
+
+     
+    /**
+     * Initialize board, deck, players
+     */
+    public Game(int _numPlayers) {
+        board = new Board();
+        deck = new Deck();
+        intro();
+        initPlayers();
+    }
+    
+    /**
+     * Play the game, announcing the winner at the end.
+     */
+    public void play() {
+        int nextPlayer = 0;
+        while (true) {
+            if (!playDomino(players[nextPlayer])) {
+                announceWinner(players[otherPlayer(nextPlayer)]);
+                return;
+            }
+            nextPlayer = otherPlayer(nextPlayer);
+        }
+    }
+
+    // Return index of other player
+    // only works for 2 players
+    private int otherPlayer(int nextPlayer) {
+        return 1 - nextPlayer;
+    }
+    
+    /**
+     * Get player to place a domino on the board
+     * @return true if the player successfully makes a play, false if not
+     */
+    boolean playDomino(Player player) {
+        int domToPlay;
+        String quit;
+        int row, col;
+        Direction dir;
+        int tries = MAX_TRIES;
+        boolean successful = false;
+        while (tries > 0) {
+            System.out.println(board);
+            System.out.println(player);
+            System.out.print("Enter " + QUIT + " to quit, any other nonblank to continue: ");
+            quit = stdin.next().toUpperCase();
+            if (quit.equals(QUIT))
+                return false;
+            domToPlay = getValidIndex(0, player.getHandSize());
+            System.out.print("Please pick row to place (left end of) domino: ");
+            row = stdin.nextInt();
+            System.out.print("Please pick column to place (left end of) domino: ");
+            col = stdin.nextInt();
+            System.out.print("Please pick direction to place domino (LEFT, UP, RIGHT, DOWN): ");
+            dir = getValidDirection();
+            if (board.place(player.get(domToPlay), row, col, dir)) {
+                player.remove(domToPlay);
+                return true;
+            }
+            // To get more helpful error messages, using an exception
+            // would be convenient
+            System.out.println("Not a legal move. Attempts left = " + --tries);
+        }
+        return false;
+    }
+
+        /**
+     * Get a valid direction from the user
+     * @return the direction
+     */
+    private Direction getValidDirection() {
+        boolean valid = false;
+        Direction result = Direction.DOWN;
+        
+        while (!valid) {
+            try {
+                result = Direction.valueOf(stdin.next());
+                valid = true;
+            }
+            catch(IllegalArgumentException exc) {
+                System.out.println("Valid directions are: LEFT, UP, RIGHT, DOWN");
+            }
+        }
+        return result;
+    }
+    
+    /**
+     * Get a valid index from the user
+     * @param low   minimum valid value
+     * @param high  1 more than the maximum valid value
+     * @return index of domino chosen
+     */
+    private int getValidIndex(int low, int high) {
+        int domToPlay = -1;
+        boolean valid = false;
+
+        while (!valid) {
+            System.out.print("Please choose the index of the domino you wish to play: ");
+            domToPlay = stdin.nextInt();
+            valid = low <= domToPlay && domToPlay < high;
+        }
+        return domToPlay;
+    }
+
+    // Announce winning player's name
+    private void announceWinner(Player winningPlayer) {
+        System.out.println(winningPlayer.getName() + " has won!");
+    }
+
+    // Display welcome message
+    private void intro() {
+        System.out.println("This is a console-based version of Néstor Romeral Andrés's Heptalion");
+        System.out.println();
+        System.out.println("For each turn, a player tries to place one of his/her dominoes on the board");
+        System.out.println("The last player to make a legal move wins");
+        System.out.println("Unlike the real game, a player loses when they either quit");
+        System.out.println("  or attempt 3 consecutive illegal moves");
+        System.out.println();
+    }
+
+    // Initialize players with their dominoes
+    private void initPlayers() {
+        players = new Player[NUM_PLAYERS];
+        String name;
+        deck.shuffle();
+        
+        for (int playerIndex = 0; playerIndex < NUM_PLAYERS;
+                playerIndex++) {
+            System.out.print("Please enter the name of player " + playerIndex + ": ");
+            name = stdin.next();
+            players[playerIndex] = new Player(name, board);
+        }
+        dealDominoes();
+
+    }
+
+    // Assign dominoes to each player
+    private void dealDominoes() {
+        Domino nextDom = deck.draw();
+        int nextPlayer = 0;
+        while (nextDom != null) {
+            players[nextPlayer].addDomino(nextDom);
+            nextPlayer = otherPlayer(nextPlayer);
+            nextDom = deck.draw();
+        }
+        
+    }
+    
+    /**
+     * Start up game
+     * @param args command line args are not used
+     */
+    public static void main(String[] args) {
+        Game heptalion = new Game(NUM_PLAYERS);
+        heptalion.play();
+    }
+}
---
+++ heptalionConsoleMVC/heptalion/Board.java
@@ -0,0 +1,119 @@
+package heptalionConsoleMVC.heptalion;
+
+import com.sun.javafx.scene.traversal.Direction;
+
+/**
+ * The standard Heptalion board
+ * @author David
+ */
+public class Board {
+    private final Square BLANK_SQ = new Square(Symbol.BLANK);
+    /* For early testing, the following 3x3 board was used
+    private final Square[][] board = {
+        {BLANK_SQ, new Square(Symbol.CROSS), BLANK_SQ},
+        {new Square(Symbol.CIRCLE), new Square(Symbol.DIAMOND), new Square(Symbol.SQUARE)},
+        {new Square(Symbol.DOT), new Square(Symbol.DIAMOND), new Square(Symbol.DIAMOND)}
+    };
+    */
+    // The complete 11 x 11 board
+    private final Square[][] board = {
+        {BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ, new Square(Symbol.CROSS), BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ},
+        {BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ, new Square(Symbol.CIRCLE), new Square(Symbol.DIAMOND), new Square(Symbol.SQUARE), BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ},
+        {BLANK_SQ, BLANK_SQ, BLANK_SQ, new Square(Symbol.CROSS), new Square(Symbol.DOT), new Square(Symbol.DIAMOND), new Square(Symbol.DIAMOND), new Square(Symbol.DOT), BLANK_SQ, BLANK_SQ, BLANK_SQ},
+        {BLANK_SQ, BLANK_SQ, new Square(Symbol.CLOVER), new Square(Symbol.DOT), new Square(Symbol.DOT), new Square(Symbol.CROSS), new Square(Symbol.DIAMOND), new Square(Symbol.CLOVER), new Square(Symbol.CIRCLE), BLANK_SQ, BLANK_SQ},
+        {BLANK_SQ, new Square(Symbol.DOT), new Square(Symbol.SQUARE), new Square(Symbol.DOT), new Square(Symbol.PLUS), BLANK_SQ, new Square(Symbol.PLUS), new Square(Symbol.CLOVER), new Square(Symbol.CLOVER), new Square(Symbol.CLOVER), BLANK_SQ},
+        {new Square(Symbol.CIRCLE), new Square(Symbol.SQUARE), new Square(Symbol.SQUARE), new Square(Symbol.DIAMOND), BLANK_SQ, BLANK_SQ, BLANK_SQ, new Square(Symbol.CIRCLE), new Square(Symbol.SQUARE), new Square(Symbol.SQUARE), new Square(Symbol.DIAMOND)},
+        {BLANK_SQ, new Square(Symbol.CLOVER), new Square(Symbol.SQUARE), new Square(Symbol.CIRCLE), new Square(Symbol.DOT), BLANK_SQ, new Square(Symbol.SQUARE), new Square(Symbol.CROSS), new Square(Symbol.CROSS), new Square(Symbol.PLUS), BLANK_SQ},
+        {BLANK_SQ, BLANK_SQ, new Square(Symbol.CROSS), new Square(Symbol.CIRCLE), new Square(Symbol.CIRCLE), new Square(Symbol.SQUARE), new Square(Symbol.PLUS), new Square(Symbol.PLUS), new Square(Symbol.CLOVER), BLANK_SQ, BLANK_SQ},
+        {BLANK_SQ, BLANK_SQ, BLANK_SQ, new Square(Symbol.PLUS), new Square(Symbol.CIRCLE), new Square(Symbol.PLUS), new Square(Symbol.PLUS), new Square(Symbol.DIAMOND), BLANK_SQ, BLANK_SQ, BLANK_SQ},
+        {BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ, new Square(Symbol.DIAMOND), new Square(Symbol.CLOVER), new Square(Symbol.DOT), BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ},
+        {BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ, new Square(Symbol.DIAMOND), BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ, BLANK_SQ}        
+    };
+            
+    // To make it a bit easier to figure out where a position is
+    // The ending 0 is for 10
+    private static final String TOPGUIDE = "   01234567890\n   ___________\n";
+
+    
+    /**
+     * Get String representation of entire board
+     * @return the representation
+     */
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        int rowNum = 0;
+
+        result.append(TOPGUIDE);
+        for (Square[] row : board) {
+            // Use last digit here to be consistent with digits across
+            // the top and to make it easier to line things up
+            result.append(lastDigit(rowNum++));
+            result.append("| ");
+            for (Square sq : row) {
+                result.append(sq.toString());
+            }
+            result.append('\n');
+        }
+        return result.toString();
+    }
+
+    /**
+     * Try to place domino at a specific position in a certain direction
+     * @param dom the domino to place
+     * @param row the row where to place the domino
+     * @param col the column where to place the domino
+     * @param dir the direction in which rest of domino lies
+     * @return true if placement was successful, false if not
+     */
+    public boolean place(Domino dom, int row, int col, Direction dir) {
+        int row2 = computeRow(row, dir);
+        int col2 = computeCol(col, dir);
+        if (invalidRow(row) || invalidRow(row2)
+                || invalidCol(col) || invalidCol(col2))
+            return false;
+        if (board[row][col].hasSymbol(dom.getSymbol1())
+                && board[row2][col2].hasSymbol(dom.getSymbol2())) {
+            board[row][col].markUsed();
+            board[row2][col2].markUsed();
+            return true;
+        }
+        return false;
+    }
+
+    // Return row of 2nd half of domino
+    private int computeRow(int row, Direction dir) {
+        if (dir.equals(Direction.UP))
+            return row - 1;
+        if (dir.equals(Direction.DOWN))
+            return row + 1;
+        return row;
+    }
+
+    // Return column of 2nd half of domino
+    private int computeCol(int col, Direction dir) {
+        if (dir.equals(Direction.LEFT))
+            return col - 1;
+        if (dir.equals(Direction.RIGHT))
+            return col + 1;
+        return col;
+    }
+
+    // Return true if row is not valid, false if valid
+    private boolean invalidRow(int row) {
+        return (row < 0 || row >= board.length);
+    }
+
+    // Return true if column is not valid, false if valid
+    private boolean invalidCol(int col) {
+        return (col < 0 || col >= board[0].length);
+    }
+    
+    // return last digit of number
+    private int lastDigit(int num) {
+        // Giving 10 a name here would not really make sense
+        // since it does not represent anything else
+        return num % 10;
+    }
+    
+}
