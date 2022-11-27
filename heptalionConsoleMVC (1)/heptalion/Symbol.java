e7f5f8bc26da9ac7faeb8b6e0b9b7417451883b6
Anthony Schultz <34993720+georgeschultz1998@users.noreply.github.com>
2022-11-26 19:34:57 -0500

    Trying to ad h example

---
+++ heptalionConsoleMVC (1)/heptalion/Symbol.java
@@ -0,0 +1,23 @@
+/**
+ * Symbols in Heptalion
+ * @author David
+ */
+public enum Symbol {
+    PLUS, CROSS, DOT, DIAMOND, SQUARE, CLOVER, CIRCLE, BLANK;
+    // The following array is mapped to the above list of values
+    // Any change to either should cause a check if the other is still
+    // consistent with the other.
+    // Could use better unicode characters if those are supported
+    // Basic NetBeans support is not great, though there is apparently
+    // a plugin
+    static final String repr[] = {"+", "X", ".", "D", "[", "*", "O", " "};
+
+    /**
+     * Produce single character String representation
+     * @return the String to use
+     */
+    @Override
+    public String toString() {
+        return repr[this.ordinal()];
+    }
+}
