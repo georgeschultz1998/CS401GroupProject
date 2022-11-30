95106365800bcbb2ed7d818ce72037797f7513c7
Bryan Le <ble45@horizon.csueastbay.edu>
2022-11-30 14:08:02 -0800

    made Junit tests for addCard, removeCard

---
+++ gloomhaven/modifierdeck/DeckTest.java
@@ -0,0 +1,38 @@
+package gloomhaven.modifierdeck;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    //test the functions of Deck/modDeck, but not the random functions
+    Deck d1 = new Deck();
+
+
+    @org.junit.jupiter.api.Test
+    void addCardTest() {
+        d1.addCard(2);
+        d1.addCard(1);
+        int result = d1.getSize();
+        //expect 20+2=22
+        assertEquals(result, 22);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeCardTest() {
+        d1.removeCard(2);
+        d1.removeCard(1);
+        d1.removeCard(0);
+        int result = d1.getSize();
+        //expect 20-3=17
+        assertEquals(result, 17);
+    }
+    @org.junit.jupiter.api.Test
+    void removeCardDupeTest() {
+        d1.removeCard(2);
+        d1.removeCard(2);
+        int result = d1.getSize();
+        //expect 20-1=19, since there is only 1 2 in the deck
+        assertEquals(result, 19);
+    }
+}
