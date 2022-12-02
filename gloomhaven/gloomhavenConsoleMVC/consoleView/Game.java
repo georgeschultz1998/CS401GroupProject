bdecec7034ba699fc108fb7d4a2d2ca39871372e
Anthony Schultz <34993720+georgeschultz1998@users.noreply.github.com>
2022-12-01 22:46:14 -0500

    Added storyline before first battle

--- gloomhaven/gloomhavenConsoleMVC/consoleView/Game.java
+++ gloomhaven/gloomhavenConsoleMVC/consoleView/Game.java
@@ -43,6 +43,8 @@ public class Game {
         player1.setName(userName);
         System.out.println("Welcome to Gloomhaven " + userName + "!");
         chooseClass(player1);
+        gameIntroduction();
+        quest1Introduction();
 
         List<List<String>> board = new ArrayList<List<String>>();
         Board.createBoard(board);
@@ -96,18 +98,26 @@ public class Game {
         while ((!(classChoice.equals("1"))) && (!(classChoice.equals("2"))) && (!(classChoice.equals("3"))) && (!(classChoice.equals("4"))) && (!(classChoice.equals("5"))) && (!(classChoice.equals("6")))) {
             if (classChoice.equals("E")){
                 output = "";
+                String temp = "";
+                output += "--------------------------------------------------------------------------------------------------------------------------------------------------\n";
                 output += "1 - INOX BRUTE\n";
-                output += "The Inox are a primitive and barbaric race, preferring to live in small nomadic tribes scattered across the wilderness.\n There they subsist through hunting and gather, scraping together a meager existence while fighting off the more dangerous creatures of the wilds.\n What they lack in intelligence and sophistication, they make up for with their superior strength and size, always eager to prove themselves in a challenge.\n And one should certainly take care in challenging an Inox.\n";
+                output += "The Inox are a primitive and barbaric race, preferring to live in small nomadic tribes scattered across the wilderness.\n There they subsist through hunting and gather, scraping together a meager existence while fighting off the more dangerous creatures of the wilds.\n What they lack in intelligence and sophistication, they make up for with their superior strength and size, always eager to prove themselves in a challenge.\n ";
+                output += "--------------------------------------------------------------------------------------------------------------------------------------------------\n";
                 output += "2 - ORCHID SPELLWEAVER\n";
-                output += "Orchids are an ancient, cave-dwelling race that views the happenings of the world through the perspective that their thousand-year lifetimes allow.\n At times, they can appear slow and meandering, but when something needs to be done, there is no hesitation.\n For the Orchids, life is about spending the proper amount of time for thought and reflection, so that the right decision can be made at exactly the right time.\n";
+                output += "Orchids are an ancient, cave-dwelling race that views the happenings of the world through the perspective that their\n thousand-year lifetimes allow. At times, they can appear slow and meandering, but when something needs to be done, there is no hesitation.\n For the Orchids, life is about spending the proper amount of time for thought and reflection, so that the right decision can be made at exactly the right time.\n";
+                output += "--------------------------------------------------------------------------------------------------------------------------------------------------\n";
                 output += "3 - QUATRYL TINKERER\n";
-                output += "Because of their diminutive size, Quatryls feel they have a lot to prove. From an early age, they are encourage to study as much as possible about many different subjects.\n Though you will find expert Quatryls in any field, they seem to have a particular affinity to engineering and machinery.\n Their long, delicate fingers allow them to build all manner of intricate contraptions to make life easier and augment their inferior physical strength.\n";
+                output += "Because of their diminutive size, Quatryls feel they have a lot to prove. From an early age, they are encourage to study\n as much as possible about many different subjects. Though you will find expert Quatryls in any field, they seem to have a particular affinity to engineering and machinery. \n Their long, delicate fingers allow them to build all manner of intricate contraptions to make life easier and augment their inferior physical strength.\n";
+                output += "--------------------------------------------------------------------------------------------------------------------------------------------------\n";
                 output += "4 - SAVVAS CRAGHEART\n";
-                output += "The Savvas value power above all else. This severe has rocky, uneven skin except for their chests, which appear as smooth, transparent glass.\n Beneath the surface of that glass is the manifestation of their power - energy cores fashioned from the elements they have mastered.\n Ice, fire, air and earth - before a Savvas masters at least one of these elements and obtains its power as their own, they are nothing.\n";
+                output += "The Savvas value power above all else. This severe has rocky, uneven skin except for their chests, which appear as smooth,\n transparent glass. Beneath the surface of that glass is the manifestation of their power - energy cores fashioned from the elements they have mastered.\n Ice, fire, air and earth - before a Savvas masters at least one of these elements and obtains its power as their own, they are nothing.\n";
+                output += "--------------------------------------------------------------------------------------------------------------------------------------------------\n";
                 output += "5 - VERMLING MINDTHIEF\n";
-                output += "Vermlings are a scavenging, animalistic race. They feed off the flesh of the dead, and when they can't find any of that, they are more than happy to do the killing themselves.\n Though primitive, they typically hunt with crude knives and bows. They are small and weak-willed, so can be controlled by more powerful races with the right combination of food and fear.\n Left to their own devices, Vermlings form hunting packs and spend their short lives fulfilling basic needs.\n";
+                output += "Vermlings are a scavenging, animalistic race. They feed off the flesh of the dead, and when they can't find any of that,\n they are more than happy to do the killing themselves. Though primitive, they typically hunt with crude knives and bows.\n They are small and weak-willed, so can be controlled by more powerful races with the right combination of food and fear. \n";
+                output += "--------------------------------------------------------------------------------------------------------------------------------------------------\n";
                 output += "6 - HUMAN SCOUNDREL\n";
-                output += "Humans are by far the most dominant of the races, spreading across across the world like a plague, erecting bloated cities and disturbing slumbering forces they can never hope to understand.\n The human society is one of rules and regulations, but also one of great diversity. Due to their intense curiosity and relentless spirit, humans can find themselves walking almost any path imaginable - from the obscenely wealthy noble to the scoundrel living in the shadows;\n ";
+                output += "Humans are by far the most dominant of the races, spreading across across the world like a plague, erecting bloated cities\n and disturbing slumbering forces they can never hope to understand. The human society is one of rules and regulations, but also one of great diversity.\n Due to their intense curiosity and relentless spirit, humans can find themselves walking almost any path imaginable.\n ";
+                output += "--------------------------------------------------------------------------------------------------------------------------------------------------\n";
                 System.out.println(output);
             }
             if (classChoice.equals("M")){
@@ -142,6 +152,50 @@ public class Game {
 
     }
 
+    public static void gameIntroduction() {
+        String output = "";
+        output += "---------------------------------------------------------------------------------------\n";
+        output += "ENTERING GLOOMHAVEN \n";
+        output += "You approach the gates of the city, looking for a fresh start.\n" +
+                "The guards stop you, asking for whence you came and your reasons for entering. \n" +
+                "You explain yourself well enough to the guards to allow yourself entry into the city,\n" +
+                " and also inquire for directions to the nearest pub. From wherever you came from, the \n" +
+                "town does not really mind your background, for here you are a nobody. You look around \n" +
+                "and see the surrounding races: humans, Quatryl, Savvas, Inox, and Valraths. Through \n" +
+                "closer inspection you can also find some Vermlings scampering about or a few Orchid \n" +
+                "groups keeping to themselves. As with most starting adventures, the best place to get \n" +
+                "a lead is usually the pub. Seeing as you are fresh from your journey and have no real \n" +
+                "reputation in the city, this is an easy way to begin your stay in Gloomhaven.\n";
+        output += "---------------------------------------------------------------------------------------\n";
+        System.out.println(output);
+        System.out.println("Press Enter to continue story: ");
+    }
+    public static void quest1Introduction() {
+        Scanner scan = new Scanner(System.in); // Create a Scanner object
+        String enter = scan.nextLine();
+        String output = "";
+        output += "---------------------------------------------------------------------------------------\n";
+        output += "GRAVEYARD QUEST \n";
+        output += "You open the pub doors and walk into the familiar aroma of booze, food, and common chatter. \n" +
+                "You take a seat at the bar and ask the bartender for a pint of mead and where a vagabond may \n" +
+                "find work in this part of town. Overhearing your question, a man on the other side of the bar \n" +
+                "inquires your skills as a warrior.I got a job for you if you know how to swing a sword and \n" +
+                "keep your nose to the ground. Surely a reasonable request of someone such as yourself.\n" +
+                "The town leader has offered to pay you twenty gold to investigate a string of recent missing \n" +
+                "persons in the city of Gloomhaven. After further investigation, you discover that all the recent \n" +
+                "missing persons were traveling merchants who regularly traveled near the Overgrown Graveyard. \n";
+        System.out.println(output);
+        System.out.println("Press Enter to continue story: ");
+        String enter2 = scan.nextLine();
+        output = "";
+        output += "- - - 1 - - -\n" +
+                "You visit the overgrown graveyard surrounding area to discover why people go missing near it.\n" +
+                "When near it, a group of two living undead surprise attack you!\n";
+        System.out.println(output);
+        System.out.println("---------------------------------------------------------------------------------------\n");
+        System.out.println("Press Enter to begin battle with enemies: ");
+        String enter3 = scan.nextLine();
+    }
 
 
         /**
