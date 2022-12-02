40d1c61af7ac8a0c92ac56b88fee0989ea1301a8
Anthony Schultz <34993720+georgeschultz1998@users.noreply.github.com>
2022-12-01 23:47:12 -0500

    Added storyline before first battle

---
+++ gloomhaven/gloomhavenConsoleMVC/gloomhaven/Hexagon.java
@@ -0,0 +1,28 @@
+package gloomhaven.gloomhavenConsoleMVC.gloomhaven;
+
+/**
+ * Hexagon on Gloomhaven board
+ * @author Anthony Schultz
+ */
+public class Hexagon {
+    private final Symbol value;
+
+    /**
+     * Initialize square
+     * @param _value initial symbol of this square
+     */
+    public Hexagon(Symbol _value) {
+        value = _value;
+    }
+
+
+    /**
+     * Get representation of one square for use
+     * in displaying board
+     * @return the representation
+     */
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
